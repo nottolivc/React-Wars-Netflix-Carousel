86d830df6d85be629d729d715a0fede0ecd72730
Andrew Nottoli <nottolivc@gmail.com>
2019-09-14 18:26:33 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Slider from './components/NetflixSlider'
+import './App.scss'
+{/* a big thanks to andrzejewsky for the original slider code refactored for this carousel */}
+const movies = [
+  {
+    id: 1,
+    image: './images/starwars0.jpg',
+    imageBg: './images/starwars1.jpg',
+    title: 'React Star Wars'
+  },
+  {
+    id: 2,
+    image: './images/starwars2.jpg',
+    imageBg: './images/starwars3.jpg',
+    title: 'React Star Wars'
+  },
+  {
+    id: 3,
+    image: './images/starwars4.jpg',
+    imageBg: './images/starwars5.jpg',
+    title: 'React Star Wars'
+  },
+  {
+    id: 4,
+    image: './images/starwars0.jpg',
+    imageBg: './images/starwars6.jpg',
+    title: 'Darth Vader'
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Slider>
+          {movies.map(movie => (
+            <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
+}
+
+export default App;
